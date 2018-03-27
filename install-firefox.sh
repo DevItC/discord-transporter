a635e5b392cbf8498da9d48c6a65dba2af952fc1
Sumit Ghosh <sumit@sumit-ghosh.com>
2018-03-27 16:31:24 +0530

    Add firefox installation script

---
+++ install-firefox.sh
@@ -0,0 +1,6 @@
+apt-get remove iceweasel
+apt-get remove firefox-esr
+echo -e "\ndeb http://downloads.sourceforge.net/project/ubuntuzilla/mozilla/apt all main" | tee -a /etc/apt/sources.list > /dev/null
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com C1289A29
+apt-get update
+apt-get install firefox-mozilla-build
